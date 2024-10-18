2f228258f3251ea4295693f846a2ecb5702cdba9
Juanramirez0523 <145750262+Juanramirez0523@users.noreply.github.com>
2024-10-18 18:40:48 -0500

    1. Creacion secciones inicio,servicios y blog

---
+++ ProyectoWeb/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.blog,name='Blog'),
+    path('categorias/<int:categoria_id>/',views.categoria,name='Categorias'),
+
+]
